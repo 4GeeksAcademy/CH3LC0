3377c22268fba7a4e0f6c008f126acd9532ef140
M0RT1M3R1694 <128715892+M0RT1M3R1694@users.noreply.github.com>
2023-09-30 03:47:50 +0000

    colores

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,18 +8,15 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>HOME PARTE DE ARRIBA</h1>
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			<div className="text-center mt-5">
+			<h1>HOME PARTE DEL CENTRO</h1>
 			</div>
 			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
+				HOME PARTE BAJA
 			</p>
 		</div>
 	);
